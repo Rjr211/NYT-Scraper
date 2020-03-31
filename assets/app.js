5dbdcbd8641e1333048b36e3f5f357b90fdb7756
Rob <robert.j1450@gmail.com>
2020-03-30 23:23:16 -0400

    setup variables, hid container

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,10 @@
 const key = "XZiML3PpnYM2SUZx5aRwvXOhL7hc9PP8";
+//search term
+let strtYr = "";
+let endYr = "";
 
+// Article container starts off as hidden
+articlecontainer.hide();
 
 fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=' + `${key}`)
   .then((response) => {
